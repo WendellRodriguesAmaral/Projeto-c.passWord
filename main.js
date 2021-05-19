c591ebbb73717f817043514f6e0543f674f7ec6a
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-05-19 00:24:20 -0300

    📦 refactor: standardizing variable names

--- main.js
+++ main.js
@@ -1,49 +1,50 @@
-const Esp =  `!@#$%¨&*_-+º^~.;/?`
-const Num = `1234567890`
-let Ltr = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`
-let newPass = ""
-const PWheight = document.querySelector("input#Tam")
-const span = document.querySelector("span#TamValue")
-const cheks = [...document.querySelectorAll("input[type=checkbox]")]
-const button =  document.querySelector("button")
-let res = document.querySelector("input#res")
-let powerPW = document.querySelector("small#powerPW")
+const specialCharacters = `!@#$%¨&*_-+º^~.;/?`;
+const numberCharacters = `1234567890`;
+const letters = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`;
 
+let generatedPassword = "";
 
-PWheight.addEventListener("input" , () => span.innerHTML = PWheight.value )
+const $passwordLength = document.querySelector("input#Tam");
+const $currentPasswordLength = document.querySelector("span#TamValue");
+const $checkboxes = [...document.querySelectorAll("input[type=checkbox]")];
+const $generate = document.querySelector("button");
 
-function power(arr){
-    if(arr.length === 0){
-        return ""
-    } else{
-        if(arr.length == 1){
-            powerPW.innerHTML = "SENHA FRACA"
-            powerPW.style.color = "red"
-        } else if(arr.length == 2){
-            powerPW.innerHTML = "SENHA MÉDIA"
-            powerPW.style.color = "#c28e00"
-        } else{
-            powerPW.innerHTML = "SENHA FORTE"
-            powerPW.style.color = "green"
-        }
+let $generatedPassword = document.querySelector("input#res");
+let $passwordStatus = document.querySelector("small#powerPW");
+
+$passwordLength.addEventListener("input", () => ($currentPasswordLength.innerHTML = $passwordLength.value));
+
+function power(arr) {
+  if (arr.length === 0) {
+    return "";
+  } else {
+    if (arr.length == 1) {
+      $passwordStatus.innerHTML = "SENHA FRACA";
+      $passwordStatus.style.color = "red";
+    } else if (arr.length == 2) {
+      $passwordStatus.innerHTML = "SENHA MÉDIA";
+      $passwordStatus.style.color = "#c28e00";
+    } else {
+      $passwordStatus.innerHTML = "SENHA FORTE";
+      $passwordStatus.style.color = "green";
     }
+  }
 }
 
-button.addEventListener("click", (b)=>{
-    newPass=""
-    b.preventDefault()
-    const pass = cheks.filter(input=>input.checked)
-                        .map(input => input.dataset.value)                            
+$generate.addEventListener("click", (event) => {
+  generatedPassword = "";
+  event.preventDefault();
+  const pass = $checkboxes.filter((input) => input.checked).map((input) => input.dataset.value);
 
-    pass.includes("Num") ? newPass+= Num:""
-    pass.includes("Esp") ? newPass+= Esp:""
-    pass.includes("Ltr") ? newPass+= Ltr:""
+  pass.includes("Num") ? (generatedPassword += numberCharacters) : "";
+  pass.includes("Esp") ? (generatedPassword += specialCharacters) : "";
+  pass.includes("Ltr") ? (generatedPassword += letters) : "";
 
-    let finalPass=""
-    for(let i=0; i< PWheight.value;i++){
-        // chatAt retorna o elemento da posição passada
-        finalPass += newPass.charAt(Math.floor(Math.random()*newPass.length)) 
-    }
-    res.value = finalPass
-    power(pass)
-})
+  let finalPass = "";
+  for (let i = 0; i < $passwordLength.value; i++) {
+    // chatAt retorna o elemento da posição passada
+    finalPass += generatedPassword.charAt(Math.floor(Math.random() * generatedPassword.length));
+  }
+  $generatedPassword.value = finalPass;
+  power(pass);
+});
